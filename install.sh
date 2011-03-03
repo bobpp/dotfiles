6c5c49a9a6b85c7fce31f4e1cd774d782a595b79
bobpp <bobpp.asroma+github@gmail.com>
2011-03-03 12:28:21 +0900

    install.sh modify
     * add .ackrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 THIS=`pwd`
-for i in screenrc vimrc zshrc gvimrc my.cnf vim ctags bashrc shrc gitconfig
+for i in screenrc vimrc zshrc gvimrc my.cnf vim ctags bashrc shrc gitconfig ackrc
 do
   ln -s $THIS/$i $HOME/.$i
 done
