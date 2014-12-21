a4f05df18e3c25a4146d89dc2f898edf45468710
bobpp <bobpp.asroma+github@gmail.com>
2014-12-21 19:55:16 +0900

    Karabiner configure

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 THIS=`pwd`
+git submodule update --init --recursive
 
 # links
-for i in screenrc vimrc zshenv zshrc gvimrc my.cnf vim ctags bashrc shrc ackrc module-starter alias oh-my-zsh oh-my-zsh-custom xvimrc peco
+for i in screenrc vimrc zshenv zshrc gvimrc my.cnf vim ctags bashrc shrc ackrc module-starter alias oh-my-zsh oh-my-zsh-custom xvimrc peco zsh
 do
   if [ -e $HOME/.$i ] ; then
     echo "Already exists $HOME/.$i"
@@ -30,4 +31,20 @@ git config --global color.ui auto
 git config --global core.excludesfile ~/.gitignore
 git config --global github.user bobpp
 echo "please execute 'git config --global github.token <MY GITHUB TOKEN>'"
+
+# if this is Mac setting for Karabiner
+if [[ $(uname) = "Darwin" ]]; then
+  if [[ -e ~/Library/Preferences/org.pqrs.Karabiner.plist ]]; then
+    echo "Already exists Karabiner configure"
+	echo "when if you want overwrite after execute....."
+	echo "   $ rm ~/Library/Preferences/org.pqrs.Karabiner.plist"
+	echo "   $ cd ~/Library/Preferences"
+	echo "   $ ln -s $THIS/application-configure/Karabiner/org.pqrs.Karabiner.plist"
+  else
+    cwd=$(pwd)
+    cd ~/Library/Preferences/
+    ln -s $THIS/application-configure/Karabiner/org.pqrs.Karabiner.plist
+    cd $cwd
+  fi
+fi
 
