0b820ec295895a145593f3e5ef1150e1f441e8e6
bobpp <matsuda.tadashi@dena.jp>
2017-02-02 14:36:57 +0900

    pre_cmd をテーマでセットすると危険を感じたので hook で逃げた

--- zsh/bobpp-theme.zsh
+++ zsh/bobpp-theme.zsh
@@ -1,9 +1,13 @@
+autoload -Uz colors
 autoload -Uz vcs_info
-precmd () { vcs_info }
+autoload -Uz add-zsh-hook
+setopt prompt_subst
+
 zstyle ':vcs_info:git:*' check-for-changes false
 zstyle ':vcs_info:*' formats '(%s:%b)'
+zstyle ':vcs_info:*' actionformats '%{$fg[red]%}(!! %s:%b !!)'
 
-autoload -Uz colors
+function _precmd_vcs_info() { LANG=en_US.utf-8 vcs_info }
+add-zsh-hook precmd _precmd_vcs_info
 
-setopt prompt_subst
 PROMPT=$'%{$fg[yellow]%}(%n@%m) %{$fg[green]%}[%~] %{$fg[blue]%}%{$vcs_info_msg_0_%}%{$reset_color%}\n%(!.#.$) '
