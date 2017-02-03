93c60d6adc031914b815f45808f85c2e8ef01664
bobpp <matsuda.tadashi@dena.jp>
2017-02-03 14:51:40 +0900

    不要ファイル削除と theme 修正

--- .zsh/bobpp-theme.zsh
+++ .zsh/bobpp-theme.zsh
@@ -1,13 +1,13 @@
 autoload -Uz colors
 autoload -Uz vcs_info
-autoload -Uz add-zsh-hook
-setopt prompt_subst
-
 zstyle ':vcs_info:git:*' check-for-changes false
-zstyle ':vcs_info:*' formats '(%s:%b)'
-zstyle ':vcs_info:*' actionformats '%{$fg[red]%}(!! %s:%b !!)'
+zstyle ':vcs_info:*' formats "$fg[blue](%s:%b)"
+zstyle ':vcs_info:*' actionformats "$fg[red](!! %s:%b !!)"
 
+# NOTE: ここに precmd を定義してしまうことも考えたが、他の設定等で競合することも考慮し hook にする
 function _precmd_vcs_info() { LANG=en_US.utf-8 vcs_info }
+autoload -Uz add-zsh-hook
 add-zsh-hook precmd _precmd_vcs_info
 
-PROMPT=$'%{$fg[yellow]%}(%n@%m) %{$fg[green]%}[%~] %{$fg[blue]%}%{$vcs_info_msg_0_%}%{$reset_color%}\n%(!.#.$) '
+setopt prompt_subst
+PROMPT=$'%{$fg[yellow]%}(%n@%m) %{$fg[green]%}[%~] %{$vcs_info_msg_0_%}%{$reset_color%}\n%(!.#.$) '
--- .zsh/no-correct.zsh
+++
@@ -1,1 +0,0 @@
-alias vagrant="nocorrect vagrant"
