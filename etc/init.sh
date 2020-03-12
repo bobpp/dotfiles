93770b063c02e55277b31febdbca17afda3e4662
bobpp <bobpp.asroma+github@gmail.com>
2020-03-12 17:13:20 +0900

    support Brewfile.local

--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,21 @@
+source etc/functions.sh
+
 # Mac only
 if [[ $(uname) = "Darwin" ]]; then
+  # Homebrew
+  if [[ -z $(which brew 2>/dev/null) ]]; then
+    show_progress "Installing Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  show_progress "Installing Homebrew Applications"
+  brew bundle
+  if [[ -f $DOTPATH/Brewfile.local ]]; then
+    brew bundle --file=$DOTPATH/Brewfile.local
+  fi
+
   # Visual Studio Code
+  show_progress "Configure Visual Studio Code"
   if [[ ! -d ~/Library/Application\ Support/Code/User ]]; then
     mkdir -p ~/Library/Application\ Support/Code/User
   fi
