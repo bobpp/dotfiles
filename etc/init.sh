7b3b53db5c5eb3393582aafd61232f42c6029a14
bobpp <bobpp.asroma+github@gmail.com>
2020-09-03 18:06:15 +0900

    VS Code の設定の同期を公式機能に移管するので git 上からは削除

--- etc/init.sh
+++ etc/init.sh
@@ -13,20 +13,4 @@ if [[ $(uname) = "Darwin" ]]; then
   if [[ -f $DOTPATH/Brewfile.local ]]; then
     brew bundle --file=$DOTPATH/Brewfile.local
   fi
-
-  # Visual Studio Code
-  show_progress "Configure Visual Studio Code"
-  if [[ ! -d ~/Library/Application\ Support/Code/User ]]; then
-    mkdir -p ~/Library/Application\ Support/Code/User
-  fi
-  for file in $(ls $DOTPATH/application-configure/VisualStudioCode.Mac/*.json); do
-    ln -sfnv $file ~/Library/Application\ Support/Code/User
-  done
-  if [[ $(which code) ]]; then
-    for extension in $(cat $DOTPATH/application-configure/VisualStudioCode.Mac/extensions); do
-      code --install-extension $extension
-    done
-  else
-    echo "Install Visual Studio Code, console tools and run `make initialize`"
-  fi
 fi
