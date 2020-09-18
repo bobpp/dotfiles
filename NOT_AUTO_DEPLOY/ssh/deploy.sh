a8490a392397dcc9f9fe3736297630b22aa5855b
bobpp <bobpp.asroma+github@gmail.com>
2020-09-18 11:09:32 +0900

    deploy to Idempotent

--- NOT_AUTO_DEPLOY/ssh/deploy.sh
+++ NOT_AUTO_DEPLOY/ssh/deploy.sh
@@ -4,7 +4,9 @@ SSH_DOTPATH="$DOTPATH/NOT_AUTO_DEPLOY/ssh"
 show_progress "Deploy SSH configures"
 
 set -eux
-mkdir "$HOME/.ssh/conf.d"
+if [ ! -d "$HOME/.ssh/conf.d" ]; then
+  mkdir "$HOME/.ssh/conf.d"
+fi
 ln -sf "$SSH_DOTPATH/.ssh/config" $HOME/.ssh/config
 for path in $(find "$SSH_DOTPATH/.ssh/conf.d/" -type f | grep -v '.keep')
 do
