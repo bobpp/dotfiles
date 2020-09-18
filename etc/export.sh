2d9b1a060465a491b26d346ba94c2c325fa91661
bobpp <bobpp.asroma+github@gmail.com>
2020-09-18 11:10:41 +0900

    iTerm2 の設定を mackup によって管理するようにした

--- etc/export.sh
+++ etc/export.sh
@@ -1,17 +1,4 @@
 # Mac only
 if [[ $(uname) = "Darwin" ]]; then
-  # Visual Studio Code
-  if [[ $(which code) ]]; then
-    code --list-extensions  > $DOTPATH/application-configure/VisualStudioCode.Mac/extensions
-  fi
-  if [[ -d ~/Library/Application\ Support/Code/User/ ]]; then
-    pushd ~/Library/Application\ Support/Code/User
-      for file_name in $(ls *.json); do
-        if [[ ! -e $DOTPATH/application-configure/VisualStudioCode.Mac/$file_name ]]; then
-          cp $file_name $DOTPATH/application-configure/VisualStudioCode.Mac/$file_name
-          ln -sfnv $DOTPATH/application-configure/VisualStudioCode.Mac/$file_name ~/Library/Application\ Support/Code/User
-        fi
-      done
-    popd
-  fi
+  mackup backup --force
 fi
