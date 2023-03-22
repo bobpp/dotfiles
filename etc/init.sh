0b8a6dd6c2715c413743e59472d1ba7f1d68c424
bobpp <bobpp.asroma+github@gmail.com>
2023-03-22 15:05:40 +0900

    homebrew path

--- etc/init.sh
+++ etc/init.sh
@@ -6,10 +6,20 @@ if [[ $(uname) = "Darwin" ]]; then
   if [[ -z $(which brew 2>/dev/null) ]]; then
     show_progress "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    show_progress "Load Homebrew envs"
+    # Intel Mac
+    if [[ -e /usr/local/bin/brew ]]; then
+      eval $(/usr/local/bin/brew shellenv)
+    fi
+    # Apple Silicon Mac
+    if [[ -e /opt/homebrew/bin/brew]]; then
+      eval $(/opt/homebrew/bin/brew shellenv)
+    fi
   fi
 
   show_progress "Installing Homebrew Applications"
-  brew bundle
+  brew bundle --file=$DOTPATH/Brewfile
   if [[ -f $DOTPATH/Brewfile.local ]]; then
     brew bundle --file=$DOTPATH/Brewfile.local
   fi
