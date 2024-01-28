adbf23f099dd5a789f7049bda4f729abd6bcbc4b
bobpp <bobpp.asroma+github@gmail.com>
2024-01-29 02:11:04 +0900

    format

--- .zsh/iterm-title.zsh
+++ .zsh/iterm-title.zsh
@@ -1,18 +1,18 @@
 function iterm-title() {
-	if [[ $(uname) != "Darwin" ]]; then
-		echo "Cant use this on not Darwin!"
-		return
-	fi
+  if [[ $(uname) != "Darwin" ]]; then
+    echo "Cant use this on not Darwin!"
+    return
+  fi
 
-	if [[ ! -v ITERM_PROFILE ]]; then
-		echo "Cant use this not use iTerm"
-		return
-	fi
+  if [[ ! -v ITERM_PROFILE ]]; then
+    echo "Cant use this not use iTerm"
+    return
+  fi
 
-	if [[ -v STY ]]; then
-		echo "Cant use this on GNU screen"
-		return
-	fi
+  if [[ -v STY ]]; then
+    echo "Cant use this on GNU screen"
+    return
+  fi
 
-	echo -ne "\033]0;${1}\007"
+  echo -ne "\033]0;${1}\007"
 }
